42a3cb9f349d157541c51aaafecad4ebd9a9794c
Christopher del Rosario <49501729+t4n6a1ka@users.noreply.github.com>
2019-09-16 05:03:26 +0800

    Update ensure.go

--- cmd/dep/ensure.go
+++ cmd/dep/ensure.go
@@ -16,11 +16,11 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/golang/dep"
-	"github.com/golang/dep/gps"
-	"github.com/golang/dep/gps/paths"
-	"github.com/golang/dep/gps/pkgtree"
-	"github.com/golang/dep/gps/verify"
+	"github.com/t4n6a1ka/dep"
+	"github.com/t4n6a1ka/dep/gps"
+	"github.com/t4n6a1ka/dep/gps/paths"
+	"github.com/t4n6a1ka/dep/gps/pkgtree"
+	"github.com/t4n6a1ka/dep/gps/verify"
 	"github.com/pkg/errors"
 )
 
